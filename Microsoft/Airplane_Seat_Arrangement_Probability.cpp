2170337eb924d86995880faf7a0c004949eaa95e
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-02 20:08:44 +0530

    Create Airplane_Seat_Arrangement_Probability.cpp

---
+++ Microsoft/Airplane_Seat_Arrangement_Probability.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        if(n==1){return (double)1;}
+        return (double)1/2;
+    }
+};
