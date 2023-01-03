c581482ee2c3d4ed4d29bb997687a46b62967625
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-03 12:26:41 +0530

    Create Number_of_substrings_containing_all_the_characters.cpp

---
+++ Microsoft/Number_of_substrings_containing_all_the_characters.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int cnt=0, j=0;
+        vector<int>v(3, 0);
+        for(int i=0; i<s.size(); i++){
+            v[s[i]-'a']++;
+            while(v[0] && v[1] && v[2]){
+                cnt+=s.size()-i;
+                v[s[j]-'a']--;
+                j++;
+            }
+        }
+        return cnt;
+    }
+};
