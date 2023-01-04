8316996235c25b71185952d99a1bdeaa129b0e7e
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-04 21:38:56 +0530

    Create Number_of_Pairs_Satisfying_Inequality.cpp

---
+++ Microsoft/Number_of_Pairs_Satisfying_Inequality.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void mergeSort(vector<int> &nums, int i, int j, long long &ans, int diff) {
+        if (i>=j) return;
+        int mid=(i+j)/2;      
+        mergeSort(nums, i, mid, ans, diff);
+        mergeSort(nums, mid + 1, j, ans, diff);
+        int idx=i;
+        for (int k=mid+1; k<=j; k++) {
+            while (idx<=mid && nums[idx]<=nums[k]+diff) {
+                idx++;
+            }
+            ans+=(idx-i);
+        }
+        inplace_merge(nums.begin()+i, nums.begin() + mid+1, nums.begin()+j+1);
+    }  
+    long long numberOfPairs(vector<int>& nums1, vector<int>& nums2, int diff) {
+        for (int i=0; i<nums1.size(); i++){
+            nums1[i]-=nums2[i];
+        }
+        long long ans=0;
+        mergeSort(nums1, 0, nums1.size() - 1, ans, diff);
+        return ans;
+    }
+    
+};
