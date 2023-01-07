3fafd4f716a5e6e1ef6df00cd93c07607444cdd4
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-08 03:52:37 +0530

    Create Split_Array_into_Consecutive_Subsequences.cpp

---
+++ Goldman Sachs/Split_Array_into_Consecutive_Subsequences.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        map<int, int>m, m1;
+        for(int i=0; i<nums.size(); i++){
+            m[nums[i]]++;
+        }
+        for(auto it: nums){
+            if(!m[it]){continue;}
+            if(m1[it]){
+                m1[it]--; m1[it+1]++;
+                m[it]--;
+            }
+            else{
+                if(m[it] && m[it+1] && m[it+2]){
+                    m[it]--; m[it+1]--; m[it+2]--;
+                    m1[it+3]++;
+                }
+                else{return false;}
+            }
+        }
+        return true;
+    }
+};
