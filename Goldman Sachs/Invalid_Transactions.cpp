1a0051e4c9a9fe5470d2442219d2b7d0ec77eac1
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-10 13:23:52 +0530

    Create Invalid_Transactions.cpp

---
+++ Goldman Sachs/Invalid_Transactions.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+   vector<string> invalidTransactions(vector<string>& transactions) {		
+        int flag=0;
+        vector<string>name;
+        vector<int>time;
+        vector<int>amount;
+        vector<string>city;
+        vector<string>res;      
+        for(auto str:transactions){
+            stringstream ss(str);
+            string st;
+            while(getline(ss,st,',')){
+                if(flag==0){
+                    name.emplace_back(st);
+                    flag=1;
+                }
+                else if(flag==1){
+                    time.emplace_back(stoi(st));
+                    flag=2;
+                }
+                else if(flag==2){
+                    amount.emplace_back(stoi(st));
+                    flag=3;                    
+                }
+                else if(flag==3){
+                    city.emplace_back(st);
+                    flag=0;                    
+                }
+            }                    
+        }
+        int j=0;
+        for(auto it:amount){
+            if(it>1000){
+                res.emplace_back(transactions[j]);                
+            }
+            j++;
+        }
+        
+        for(int i=0;i<transactions.size();i++){
+            for(int j=0;j<transactions.size();j++){
+                if(name[i]==name[j] && city[i]!=city[j]){
+                    if(abs(time[i]-time[j])<=60){
+                        if(amount[i]<=1000){
+                            res.emplace_back(transactions[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+		
+        return res;
+    }
+};
