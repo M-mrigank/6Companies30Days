f8010ab8da6d8334bc020db5b89a49c63ddcd81a
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-22 01:42:28 +0530

    Create Random_Pick_with_Weight.cpp

---
+++ Google/Random_Pick_with_Weight.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    vector<int> m;
+    random_device rd;
+public:
+    Solution(vector<int> w) {
+        m = move(w);
+        for (int i = 1; i < m.size(); ++i)
+            m[i] += m[i-1];
+    }
+    int pickIndex() {
+        return lower_bound(m.begin(), m.end(), rd() % m.back() + 1) - m.begin();
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(w);
+ * int param_1 = obj->pickIndex();
+ */
