61f6344e832083e43f65a3bb655a7d3f83adc231
M-mrigank <92224747+M-mrigank@users.noreply.github.com>
2023-01-30 00:36:02 +0530

    Create Dota2_Senate.cpp

---
+++ Amazon/Dota2_Senate.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        if(senate.size() == 1){return (senate[0]=='R')?"Radiant":"Dire";}
+        queue<char> q1, q2;
+        for(auto i: senate){q1.push(i);}
+        while(!q1.empty()){
+            if(q2.empty() || q1.front()==q2.front()){
+                q2.push(q1.front()), q1.pop();
+            }
+            else{
+                q1.pop(), q1.push(q2.front()), q2.pop();
+            }
+        }
+        return (q2.front() == 'R')?"Radiant":"Dire";
+    }
+};
